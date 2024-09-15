81bdabda8f4b7d8eaed30be43e7bf6c33e2f9dd9
shox404 <shxxx404@gmail.com>
2024-09-15 10:32:30 +0500

    Divide js

---
+++ sr.js
@@ -0,0 +1,37 @@
+class SpeechRecognition {
+    constructor({ continuous }) {
+      this.On = window.SpeechRecognition || window.webkitSpeechRecognition;
+      if (!this.On) {
+        alert("Speech Recognition API not supported in this browser.");
+      } else {
+        this.continuous = continuous;
+      }
+    }
+    recognition() {
+      const rec = new this.On();
+      rec.continuous = this.continuous;
+      return rec;
+    }
+    start(btn, callback) {
+      const startButton = document.getElementById(btn);
+      startButton.addEventListener("click", () => {
+        rec.start();
+        logDiv.innerHTML = "<p>Listening...</p>";
+        if (callback) callback();
+      });
+    }
+    result(callback) {
+      rec.addEventListener("result", (event) => {
+        let text = this.getValue(event);
+        if (callback) return callback(text);
+      });
+    }
+    getValue(event) {
+      let text = "";
+      const { results, resultIndex } = event;
+      for (let i = resultIndex; i < results.length; i++) {
+        text += results[i][0].transcript;
+      }
+      return text;
+    }
+  }
